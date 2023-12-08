a35a403296dadfe7f918c424951ab62890be63fd
cadeja <cadejengland@proton.me>
2023-12-07 20:52:22 -0500

    boy howdy is this pretty messy but it got the job done.

--- day7/script.py
+++ day7/script.py
@@ -25,22 +25,38 @@ def flatten(list):
     return new_list
 
 def unique_cards(cards):
-    return len(set(cards))
+    # don't count jokers for part 2
+    s = set(cards)
+    if 'J' in s: 
+        return len(s) - 1
+    return len(s)
 
 def hand_type(cards):
-    unique = unique_cards(cards)
+    unique = unique_cards(cards) # does NOT include Js
+    num_of_jokers = cards.count('J')
 
     match unique:
+        case 0:
+            return hand_types['five of a kind']
         case 1:
             return hand_types['five of a kind']
         case 2:
-            if unique_cards(sorted(cards)[1:4]) == 1:
+            if num_of_jokers >= 2:
                 return hand_types['four of a kind']
-            else:
-                return hand_types['full house']
+            for item in set(cards):
+                if cards.count(item) + num_of_jokers == 4:
+                    return hand_types['four of a kind']
+            return hand_types['full house']
+            
+            # mid = unique_cards(sorted(cards)[1:4])
+            # if mid == 1 or mid == 0 or num_of_jokers >= 2:
+            #     return hand_types['four of a kind']
+            # else:
+            #     return hand_types['full house']
         case 3:
-            s = set(cards)
-            for item in s:
+            if num_of_jokers > 0:
+                return hand_types['three of a kind']
+            for item in set(cards):
                 if cards.count(item) == 2:
                     return hand_types['two pair']
                 if cards.count(item) == 3:
@@ -51,33 +67,30 @@ def hand_type(cards):
             return hand_types['high card'] 
 
 def card_values(value):
-    print(value[0])
     value = list(value[0])
     new_value = []
     for item in value:
-        print(item)
         match item:
             case 'A': new_value.append(14)
             case 'K': new_value.append(13)
             case 'Q': new_value.append(12)
-            case 'J': new_value.append(11)
+            case 'J': new_value.append(1) # 11 for part1, 1 for part2
             case 'T': new_value.append(10)
             case _: new_value.append(int(item))
-    print(new_value)
     return new_value
 
-def part1():
+
+
+def engage():
     sorted_hands = [[],[],[],[],[],[],[]]
 
     for hand in hands:
         sorted_hands[hand_type(hand[0])].append(hand)
 
     for i in range(7):
-        s = sorted(sorted_hands[i],key=card_values)
-        sorted_hands[i] = s
+        sorted_hands[i] = sorted(sorted_hands[i],key=card_values)
 
     sorted_hands = flatten(sorted_hands)
-    print(sorted_hands)
 
     winnings = 0
     for i in range(len(sorted_hands)):
@@ -85,6 +98,4 @@ def part1():
     
     print(winnings)
 
-
-        
-part1()
+engage()
